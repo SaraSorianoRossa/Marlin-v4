0598578c93658b8510e601d86b775c76585d1b1d
Sara <91597640+SaraSorianoRossa@users.noreply.github.com>
2024-04-24 21:47:21 +0200

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,8 @@ extern crate ark_std;
 
 use ark_ff::{to_bytes, PrimeField, UniformRand};
 use ark_poly::{univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain};
-use ark_poly_commit::Evaluations;
-use ark_poly_commit::{LabeledCommitment, PCUniversalParams, PolynomialCommitment};
+use ark_poly_commit_new::Evaluations;
+use ark_poly_commit_new::{LabeledCommitment, PCUniversalParams, PolynomialCommitment};
 use ark_relations::r1cs::ConstraintSynthesizer;
 use ark_std::rand::RngCore;
 use digest::Digest;
